d7b7ba3c9845b096b7504b3c881b276309bcab96
Earnest John Gungon <ej.gungon@springboardph.com>
2025-05-02 14:21:17 +0800

    Done Project

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,11 @@
 @extends('layout')
 @section('content')
     <div class="container">
-        <h1>Welcome to the App</h1>
-        <p>This is the main content area of the app.</p>
+        <div class="flex flex-col items-center justify-center h-screen gap-4">
+            <h1 class="text-4xl font-bold">Welcome to my Technical Test</h1>
+            <a href="{{ route('customers.index') }}" class="border border-gray-500 rounded-md text-black px-4 py-2 hover:bg-black hover:text-white transition-all duration-300">
+                Start Using App
+            </a>
+        </div>
     </div>
 @endsection
